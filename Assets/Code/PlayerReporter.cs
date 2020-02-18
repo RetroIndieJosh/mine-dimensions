4bcfdcd96d43bc51ab219f78a2119447023f3236
Joshua McLean <mrjoshuamclean@gmail.com>
2020-02-18 12:39:31 -0500

    Express heading as compass direction

--- Assets/Code/PlayerReporter.cs
+++ Assets/Code/PlayerReporter.cs
@@ -7,24 +7,61 @@ using TMPro;
 public class PlayerReporter : TickUpdateComponent
 {
     [Header("UI")]
+    [SerializeField] private TextMeshProUGUI m_headingText = null;
     [SerializeField] private TextMeshProUGUI m_registeringText = null;
 
     private readonly string BASE_URL = "http://104.237.13.63/asym/unity.php";
 
+    private Rigidbody m_body = null;
     private int m_id = -1;
     private bool m_isRegistered = false;
     private int m_connectTime = 0;
+
+    enum Direction
+    {
+        E = 0,
+        NE = 1,
+        N = 2,
+        NW = 3,
+        W = 4,
+        SW = 5,
+        S = 6,
+        SE = 7
+    }
+
+    private string Heading {
+        get {
+            var vel2 = new Vector2(m_body.velocity.x, m_body.velocity.z).normalized;
+            if (vel2.magnitude < Mathf.Epsilon)
+                return "--";
+
+            var angle = Mathf.Atan2(vel2.y, vel2.x);
+            var octant = Mathf.RoundToInt((8 * angle / (2 * Mathf.PI)) + 8) % 8;
+            var dir = (Direction)octant;
+
+            return $"{dir,2}";
+        }
+    }
 
     protected override void TickUpdate() {
         if (!m_isRegistered)
             return;
 
-        var x = transform.position.x;
-        var z = transform.position.z;
+        var x = transform.position.x.ToString("F2");
+        var z = transform.position.z.ToString("F2");
+        var vel2 = new Vector2(m_body.velocity.x, m_body.velocity.z);
+        var speed = vel2.magnitude.ToString("F2");
+        var normVel2 = vel2.normalized;
+        var headingX = normVel2.x.ToString("F2");
+        var headingZ = normVel2.y.ToString("F2");
         var time = Mathf.Ceil(m_connectTime + Time.time);
-        var url = BASE_URL + $"?action=add&id={m_id}&x={x}&z={z}&time={time}";
+        var url = BASE_URL + $"?action=add&id={m_id}&x={x}&z={z}&time={time}&heading='{Heading}'&head_x={headingX}&head_z={headingZ}&speed={speed}";
         var updateServer = AsymWeb.instance.Request(url);
         StartCoroutine(updateServer);
+    }
+
+    private void Awake() {
+        m_body = GetComponent<Rigidbody>();
     }
 
     protected override void Start() {
@@ -36,6 +73,12 @@ public class PlayerReporter : TickUpdateComponent
         var url = BASE_URL + "?action=user&name=bob";
         var login = AsymWeb.instance.Request(url, Register);
         StartCoroutine(login);
+    }
+
+    protected override void Update() {
+        base.Update();
+        if (m_headingText != null)
+            m_headingText.text = Heading;
     }
 
     private void Register(string a_result) {
